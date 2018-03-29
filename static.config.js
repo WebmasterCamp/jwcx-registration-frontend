0d78bf018c70be375d619ee9f3d3b7b7cb808ef2
Phoomparin Mano <work@phoom.in.th>
2018-03-29 12:21:27 +0700

    :memo: Updated Site Root to registration.jwc.in.th

--- static.config.js
+++ static.config.js
@@ -3,7 +3,7 @@ import {extractCritical} from 'emotion-server'
 
 import webpack from './webpack.config.js'
 
-const siteRoot = 'https://jwcx-196915.firebaseapp.com'
+const siteRoot = 'https://registration.jwc.in.th'
 
 class Document extends Component {
   render() {
