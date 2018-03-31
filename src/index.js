6b151a6094e0374e7a9d596c76703fcd904fc739
Pmc Dev <phoomparin@gmail.com>
2018-03-31 19:43:46 +0700

    :memo: Implement Segment Analytics

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import {AppContainer} from 'react-hot-loader'
 
 import App from './components/App'
 
+import analytics from './core/analytics'
+
 if (typeof document !== 'undefined') {
+  analytics()
+
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate
 
   const render = Component => {
