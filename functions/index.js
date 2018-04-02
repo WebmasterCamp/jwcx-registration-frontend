15a3cc9301757450a2ce3cb159030748bb0c467e
Pmc Dev <phoomparin@gmail.com>
2018-04-02 16:11:36 +0700

    :bug: :rocket: Fixed Deployment Errors in Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,20 @@ const functions = require('firebase-functions')
 
 admin.initializeApp(functions.config().firebase)
 
-exports.stats = firestore.document('campers/{camperId}').onWrite(e => {
-  const data = e.data.data()
-  const counterRef = db.collection('stats').doc('counter')
+exports.stats = functions.firestore
+  .document('campers/{camperId}')
+  .onWrite(e => {
+    const data = e.data.data()
+    const counterRef = db.collection('stats').doc('counter')
 
-  if (data.submitted) {
-    return db.transaction(transaction => {
-      return transaction.get(counterRef).then(doc => {
-        const major = data.major
-        const payload = {[major]: doc.data(major) + 1}
+    if (data.submitted) {
+      return db.transaction(transaction => {
+        return transaction.get(counterRef).then(doc => {
+          const major = data.major
+          const payload = {[major]: doc.data(major) + 1}
 
-        return transaction.update(counterRef, payload, {merge: true})
+          return transaction.update(counterRef, payload, {merge: true})
+        })
       })
-    })
-  }
-})
+    }
+  })
